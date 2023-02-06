39cceeaddab7a0611affdab37f759faf3530e7d2
Nayan Parmar <nparmar84@gmail.com>
2023-02-06 11:07:21 -0800

    added files

---
+++ religiousFishermen.js
@@ -0,0 +1,105 @@
+/*
+There are many religious fishermen at sea. Two fishermen will work together in a squad if either they have the same religion or the same fishing rod. 
+Also, if fisherman Alex is willing to work with fisherman Bob, and fisherman Bob is willing to work with fisherman Caleb, 
+then fisherman Alex and fisherman Caleb will be willing to work together (because they trust fisherman Bob).
+
+You'll be given an array where each element represents a fisherman (e.g. [['christianity', 'Johnny Morris']] means 
+we have 1 fisherman whose religion is 'christianity' and whose fishing rod is 'Johnny Morris'). 
+You must return the size of the largest squad of fishermen.
+*/
+
+const { Queue } = require('./utils/queue');
+
+const areConnected = (fishermanOne, fishermanTwo) => {
+    const [fishermanOneReligion, fishermanOneFishingRod] = fishermanOne;
+    const [fishermanTwoReligion, fishermanTwoFishingRod] = fishermanTwo;
+
+    return (
+        fishermanOneReligion === fishermanTwoReligion ||
+        fishermanOneFishingRod === fishermanTwoFishingRod
+    );
+};
+
+const buildGraph = (nodes) => {
+    const graph = {};
+    const numNodes = nodes.length;
+
+    for (let i = 0; i < numNodes; i++) {
+        for (let j = i + 1; j < numNodes; j++) {
+            const nodeOne = nodes[i];
+            const nodeTwo = nodes[j];
+
+            if (!areConnected(nodeOne, nodeTwo)) continue;
+
+            const containsNodeOne = graph.hasOwnProperty(nodeOne);
+            const containsNodeTwo = graph.hasOwnProperty(nodeTwo);
+
+            if (!containsNodeOne) graph[nodeOne] = [];
+            if (!containsNodeTwo) graph[nodeTwo] = [];
+
+            graph[nodeOne].push(nodeTwo);
+            graph[nodeTwo].push(nodeOne);
+        }
+    }
+    return graph;
+};
+
+const getNumOfNodes = (node, visited, graph) => {
+    const queue = new Queue();
+    visited.add(node);
+
+    queue.enqueue(node);
+
+    let numNodes = 0;
+    while (queue.size() > 0) {
+        // Remove node
+        const node = queue.dequeue();
+
+        // Process node
+        numNodes++;
+
+        // Get neighbors
+        const containsNodeInGraph = graph.hasOwnProperty(node);
+        if (!containsNodeInGraph) continue;
+
+        const neighbors = graph[node];
+        for (const neighbor of neighbors) {
+            if (visited.has(neighbor)) continue;
+
+            visited.add(neighbor);
+            queue.enqueue(neighbor);
+        }
+    }
+    return numNodes;
+};
+
+const findLargestSquadSize = (fishermen) => {
+    const visitedFisherman = new Set();
+    const graph = buildGraph(fishermen);
+    let numSquadFishermen = 0;
+
+    for (const fisherman of fishermen) {
+        if (visitedFisherman.has(fisherman)) continue;
+
+        const currentNumSquadFishermen = getNumOfNodes(fisherman, visitedFisherman, graph);
+        numSquadFishermen = Math.max(numSquadFishermen, currentNumSquadFishermen);
+    }
+    return numSquadFishermen;
+};
+
+const fishermen = [
+    ['Sikhism', 'Orvis'],
+    ['Christianity', 'Johnny Morris'],
+    ['Buddhism', 'Winn Grip'],
+    ['Islam', 'Shimano'],
+    ['Christianity', 'Fuji'],
+    ['Buddhism', 'Shimano'],
+    ['Christianity', 'Fuji'],
+    ['Judaism', 'Fuji'],
+    ['Christianity', 'Johnny Morris'],
+    ['Hinduism', 'Orvis'],
+];
+
+console.log(`Your answer: ${findLargestSquadSize(fishermen)}`);
+console.log(`Correct answer: ${5}`);
+console.log();
---
+++ warringBishops.js
@@ -0,0 +1,96 @@
+/*
+Imagine that we have an 8 x 8 chess board that has some bishops on it, and no other pieces. 
+In chess, bishops can travel diagonally in all 4 directions, and they can move as far as they want in these directions 
+(however, if there is another piece in the way, they cannot move past that piece).
+
+You'll be a given an array of arrays. Each of the smaller arrays has size 2, and represents 1 bishop. 
+For example, if we have, [[1,2]], then there is a bishop at row = 1, col = 2.
+
+If 2 bishops can attack each other (i.e. they are in the same diagonal), we'll say that these are warring bishops. 
+Additionally, if bishop 1 can attack bishop 2, and bishop 2 can attack bishop 3, then all 3 of these are warring bishops.
+
+Given the input list of bishops, return true if all the bishops are part of the same group of warring bishops. Otherwise, return false.
+*/
+
+const { Queue } = require('./utils/queue');
+
+const areWarring = (bishopOne, bishopTwo) => {
+    const [bishopOneRow, bishopOneCol] = bishopOne;
+    const [bishopTwoRow, bishopTwoCol] = bishopTwo;
+
+    const slope = Math.abs((bishopTwoCol - bishopOneCol) / (bishopTwoRow - bishopOneRow));
+
+    return slope === 1;
+};
+
+const buildGraph = (nodes) => {
+    const graph = {};
+    const numNodes = nodes.length;
+
+    for (let i = 0; i < numNodes; i++) {
+        for (let j = i + 1; j < numNodes; j++) {
+            const nodeOne = nodes[i];
+            const nodeTwo = nodes[j];
+
+            if (!areWarring(nodeOne, nodeTwo)) continue;
+
+            const containsNodeOne = graph.hasOwnProperty(nodeOne);
+            const containsNodeTwo = graph.hasOwnProperty(nodeTwo);
+
+            if (!containsNodeOne) graph[nodeOne] = [];
+            if (!containsNodeTwo) graph[nodeTwo] = [];
+
+            graph[nodeOne].push(nodeTwo);
+            graph[nodeTwo].push(nodeOne);
+        }
+    }
+    return graph;
+};
+
+const isComponent = (startNode, graph, visited) => {
+    const queue = new Queue();
+    visited.add(startNode);
+
+    queue.enqueue(startNode);
+
+    while (queue.size() > 0) {
+        // Remove node
+        const node = queue.dequeue();
+
+        // Process node
+
+        // Get neighbors
+        const containsNode = graph.hasOwnProperty(node);
+        if (!containsNode) continue;
+
+        const neighbors = graph[node];
+        for (const neighbor of neighbors) {
+            if (visited.has(neighbor)) continue;
+
+            visited.add(neighbor);
+            queue.enqueue(neighbor);
+        }
+    }
+};
+
+const checkIfIsGroupOfWarringBishops = (bishops) => {
+    const visitedBishop = new Set();
+    const graph = buildGraph(bishops);
+    const startBishop = bishops[0];
+
+    isComponent(startBishop, graph, visitedBishop);
+
+    return visitedBishop.size === bishops.length;
+};
+
+const bishops = [
+    [0, 4],
+    [2, 6],
+    [3, 1],
+    [6, 2],
+    [7, 5],
+];
+
+console.log(`Your answer: ${checkIfIsGroupOfWarringBishops(bishops)}`);
+console.log(`Correct answer: ${true}`);
+console.log();
